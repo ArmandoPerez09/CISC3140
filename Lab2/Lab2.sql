5128aba5a97be29593263149864b2d1b02463b3f
Armando Perez <armando123perez@gmail.com>
2022-03-08 22:45:45 -0500

    Lab2.db and Lab2.sql updated. Added extract2.csv

--- Lab2/Lab2.sql
+++ Lab2/Lab2.sql
@@ -1,6 +1,15 @@
+--Armando Perez
+--CISC3140
+--Lab2
+--3/9/22
+
 .mode csv
-.import data_lab2/data.csv
+--Import data.csv from local directory
+.import data_lab2/data.csv temp_table
 .header on
+
+--TABLE CREATION
+--cars table to hold data of car information
 CREATE TABLE cars (
 CarID varchar(255) NOT NULL PRIMARY KEY,
 Year varchar(255),
@@ -10,11 +19,14 @@ Name varchar(255),
 Email varchar(255)
 );
 
+--judges table to hold judges data
 CREATE TABLE judges (
 JudgeID varchar(255),
-JudgeName varchar(255)
+JudgeName varchar(255),
+time timestamp
 );
 
+--carScore table to hold the scores of each car
 CREATE TABLE carScore (
 CarID varchar(255) NOT NULL PRIMARY KEY,
 RacerTurbo varchar(255),
@@ -44,6 +56,17 @@ ModsWIP varchar(255),
 ModsOverall varchar(255)
 );
 
+--extract table to output data into csv files (extract1, extract2)
+CREATE TABLE extract (
+Ranking INTEGER,
+CarID varchar(255),
+Year varchar(255),
+Maker varchar(255),
+Model varchar(255)
+);
+
+--DATA INSERTION
+--Inserts data from temp_table into carScore table
 INSERT INTO carScore (
 CarID, RacerTurbo, RacerSupercharged, RacerPerformance, 
 RacerHorsepower, CarOverall, EngineMod,EnginePerformance, 
@@ -59,8 +82,57 @@ Body_Frame_Detailing, Body_Frame_Cleanliness, Mods_Paint, Mods_Body,
 Mods_Wrap, Mods_Rims, Mods_Interior, Mods_Other, Mods_ICE, 
 Mods_Aftermarket, Mods_WIP, Mods_Overall FROM temp_table;
 
-INSERT INTO judges (JudgeID, JudgeName)
-SELECT Judge_ID, Judge_Name FROM temp_table;
-
+--Inserts data from temp_table into cars table
 INSERT INTO cars (CarID, Year, Maker, Model, Name, Email)
 SELECT Car_ID, Year, Maker, Model, Name, Email FROM temp_table;
+
+--Sums all scores for a total score use for a ranking column
+INSERT INTO extract (Ranking, CarID, Year, Maker, Model)
+SELECT (Racer_Turbo+Racer_Supercharged+Racer_Performance+Racer_Horsepower+
+Car_Overall+Engine_Modifications+Engine_Performance+Engine_Chrome+
+Engine_Detailing+Engine_Cleanliness+Body_Frame_Undercarriage+
+Body_Frame_Suspension+Body_Frame_Chrome+Body_Frame_Detailing+
+Body_Frame_Cleanliness+Mods_Paint+Mods_Body+Mods_Wrap+Mods_Rims+
+Mods_Interior+Mods_Other+Mods_ICE+Mods_Aftermarket+Mods_WIP+Mods_Overall),
+ Car_Id, Year, Make, Model FROM temp_table;
+
+--Total Scores sorted in descending order
+--Exports queried data from extract table to extract1.csv 
+ .output extract1.csv
+ SELECT * FROM extract
+ ORDER BY cast(RANKING as unsigned) DESC; 
+
+--Top 3 Car Maker Ranks
+--Exports queried data from extract table to extract2.csv
+.output extract2.csv
+SELECT * FROM (
+--RANK () creates Ranking for each maker, Creates new column MakerRank
+SELECT Ranking, CarID, Year, Maker, Model, RANK () OVER (
+PARTITION BY Maker
+--All scores are sorted in descending order
+ORDER BY cast(Ranking as unsigned) DESC)
+MakerRank FROM extract)
+--Makes it so that only the top three cars are shown
+t WHERE MakerRank <=3;
+
+--Update Judges table
+--Adds columns and inserts data from temp_table
+ALTER TABLE judges
+ADD COLUMN numCars INTEGER;
+ALTER TABLE judges
+ADD COLUMN start varchar(255)
+ALTER TABLE judges
+ADD COLUMN end varchar(255)
+ALTER TABLE judges
+ADD COLUMN duration varchar(255)
+ALTER TABLE judges
+ADD COLUMN average varchar(255)
+
+INSERT INTO judges(JudgeID, JudgeName, numCar, start, end, duration, average)
+SELECT Judge_ID, Judge_Name, count(*), MIN(Timestamp), MAX(Timestamp), MAX(Timestamp)-MIN(Timestamp), AVG(Timestamp)
+FROM temp_table
+GROUP BY Judge_ID;
+
+--END OF PROGRAM
+.save Lab2.db
+.quit
